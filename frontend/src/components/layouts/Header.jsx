0f962ecd69d2537b8e08c55336e5698676051b8b
Aditya-Deuskar <adityadeuskar2003@gmail.com>
2024-07-22 20:27:03 +0530

    added search bar into the header

--- frontend/src/components/layouts/Header.jsx
+++ frontend/src/components/layouts/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import Search from './Search';
 
 export default function Header() {
-  return <nav className="navbar row sticky-top">
-    {/* logo */}
-    <div className="col-12 col-md-3">
-        <img src="/images/logo.webp" alt="logo" className="logo" />
-    </div>
-    {/* search bar and search icon */}
-    
-  </nav>;
+    return <nav className="navbar row sticky-top">
+        {/* logo */}
+        <div className="col-12 col-md-3">
+            <img src="/images/logo.webp" alt="logo" className="logo" />
+        </div>
+        {/* search bar and search icon */}
+        <div className="col-12 col-md-6 mt-2 mt-md-6">
+            <Search />
+        </div>
+
+    </nav>;
 }
---
+++ frontend/src/components/layouts/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaSearch } from "react-icons/fa";
+
+export default function Search() {
+    return <form>
+        <div className="input-group">
+            <input type='text' placeholder='search your favourite resturaunt' className="form-control" id="search_field" />
+            <div className="input-group-append">
+                <button id="search_btn" className="btn"><FaSearch className='fa fa-search' /></button>
+            </div>
+        </div>
+    </form>
+}
