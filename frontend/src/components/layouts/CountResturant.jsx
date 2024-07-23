804b84cdea18c0c24449b4a8e3bccd15aeb5fb24
Aditya-Deuskar <adityadeuskar2003@gmail.com>
2024-07-23 19:36:55 +0530

    here these changes display the number of resturants below the header. In this stage it is static right now and not dynamic.

---
+++ frontend/src/components/layouts/CountResturant.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CountResturant() {
+  return <div>
+  <p className="NumOfRestro">
+      8 <span className="Restro">Resturants</span>
+  </p>
+  <hr />
+</div>
+}
---
+++ frontend/src/components/layouts/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import CountResturant from './CountResturant'
+
+export default function Home() {
+  return <>
+  <CountResturant />
+  </>
+}
