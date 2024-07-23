7f4cac076ebf841d25503db56b342187dd373c64
Aditya-Deuskar <adityadeuskar2003@gmail.com>
2024-07-23 19:46:54 +0530

    Here we added the sorting menu, by rating/review, etc

--- frontend/src/components/layouts/Home.jsx
+++ frontend/src/components/layouts/Home.jsx
@@ -4,5 +4,14 @@ import CountResturant from './CountResturant'
 export default function Home() {
   return <>
   <CountResturant />
+  {/* Section tag is just like a div tag */}
+  <section> 
+    <div className="sort">
+        <button className="sort_veg p-3">Pure Veg</button>
+        <button className="sort_veg p-3">sort by review</button>
+        <button className="sort_veg p-3">sort by rating</button>
+
+    </div>
+  </section>
   </>
 }
