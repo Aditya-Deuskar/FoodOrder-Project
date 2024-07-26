509117b61a0f735934854f73d48a4bbc73746702
Aditya-Deuskar <adityadeuskar2003@gmail.com>
2024-07-26 20:48:35 +0530

    here we created reducer logic... which will actually take the data coming from db... and store it inside the storage area called as 'state', and the logic of state is written in state.js..... so yahase data state.js mein jayega store honeko...

---
+++ frontend/src/reducer/restaurantReducer.js
@@ -0,0 +1,30 @@
+ //the store in redux is called state here... its nothing but a data storing place... just name is state
+
+import { ALL_RESTAURANTS_REQUEST, ALL_RESTAURANTS_SUCCESS } from "../constants/restaurantConstant";
+
+ const initialState = {
+    restaurants: [],  //fetch karneke baad... restaurants ka data iss array mein collect hoga...
+};
+
+export const restaurantReducer = (state = initialState, action) =>{
+    switch (action.type) {
+        case ALL_RESTAURANTS_REQUEST:
+            return{
+                ...state,
+                loading: true,
+                error: null,
+            }; 
+
+        case ALL_RESTAURANTS_SUCCESS:
+            return{
+                ...state,
+                loading: false,
+                count: action.payload.count,
+                restaurants: action.payload,
+            }
+        
+        default:
+            return state;
+            
+    }
+}
