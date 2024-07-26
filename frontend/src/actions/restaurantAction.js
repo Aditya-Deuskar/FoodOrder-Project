e0786486f2851cb252727574a7791feef73086df
Aditya-Deuskar <adityadeuskar2003@gmail.com>
2024-07-26 20:32:14 +0530

    here we have created actions and constants

---
+++ frontend/src/actions/restaurantAction.js
@@ -0,0 +1,22 @@
+import { ALL_RESTAURANTS_REQUEST, ALL_RESTAURANTS_SUCCESS } from "../constants/restaurantConstant"
+import axios from "axios";
+// this content has the logic to take data from backend and store it inside the store.
+
+const getRestaurants = () => {
+    return async (dispatch) => {
+        try {
+            dispatch({ type: ALL_RESTAURANTS_REQUEST });
+            let link = `/api/v1/eats/stores`; //this is a link to our database
+            const { data } = await axios.get(link);
+            console.log(data);
+
+            const { restaurants, count } = data;
+            dispatch({
+                type: ALL_RESTAURANTS_SUCCESS,
+                payload: { restaurants, count },
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    };
+};
