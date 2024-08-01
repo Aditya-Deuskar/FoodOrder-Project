b3dd37193b7ee60f44abf7d900c4a28ade49f920
Aditya-Deuskar <adityadeuskar2003@gmail.com>
2024-08-01 21:05:07 +0530

    this also included in the previous one, the regisration, etc

---
+++ frontend/src/reducer/userReducer.js
@@ -0,0 +1,81 @@
+// Reducer for managing authentication related state
+
+import { clearErrors } from "../actions/restaurantAction";
+import {
+  LOAD_USER_FAIL,
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOGIN_FAIL,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGOUT_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_USER_FAIL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+} from "../constants/userConstant";
+
+export const authReducer = (
+  state = {
+    user: null,
+    loading: false,
+    isAuthenticated: false,
+    data: {},
+  },
+  action
+) => {
+  switch (action.type) {
+    case LOGIN_REQUEST:
+    case REGISTER_USER_REQUEST:
+    case LOAD_USER_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        isAuthenticated: false,
+      };
+
+    case LOGIN_SUCCESS:
+    case REGISTER_USER_SUCCESS:
+    case LOAD_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: true,
+        user: action.payload,
+      };
+
+    case LOGOUT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+      };
+
+    case LOGIN_FAIL:
+    case REGISTER_USER_FAIL:
+    case LOAD_USER_FAIL:
+      return {
+        ...state,
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+        error: action.payload,
+      };
+      
+      case LOGOUT_FAIL:
+        return{
+            ...state,
+            error: action.payload,
+        };
+
+    case clearErrors:
+      return {
+        ...state,
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
